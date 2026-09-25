b495aebe9a2772202ec724f864ed3cb7bafb4e2c
cyanvoyager <onoderatakeshi6@gmail.com>
2026-09-25 13:08:42 +0300

    aded dict

--- dictionaries.py
+++ dictionaries.py
@@ -5,8 +5,28 @@ transactions = [
     {"description": "Electricity", "amount": 12000, "type": "expense"}
 ]
 
+# total_revenue=sum(transaction["amount"] for transaction in transactions if transaction.get("type")=="revenue" )
+# total_expenses=sum(transaction["amount"] for transaction in transactions if transaction.get("type") =="expense")
+
+# profit=total_revenue-total_expenses
+
+# print(total_revenue, total_expenses)
+
+# print(f"The profit is  {profit}")
+
 total_revenue=0
 total_expenses=0
+for transaction in transactions:
+    if transaction["type"]=="revenue":
+        total_revenue=total_revenue+ transaction["amount"]
 
-for i in transactions:
-    print(i)
+    if transaction["type"]=="expense":
+        total_expenses=total_expenses+transaction["amount"]
+
+
+profit=total_revenue-total_expenses
+
+print(f"Revenue: {total_revenue} \nExpenses: {total_expenses} \nProfit: {profit}")
+
+
+
